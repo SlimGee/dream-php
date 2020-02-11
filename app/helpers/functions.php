b00222ea6be87f608394e125ef93048c224e0f8f
Given Ncube <given@front.com>
2020-02-11 16:01:20 +0200

    Updated the authenticate user helper method

--- app/helpers/functions.php
+++ app/helpers/functions.php
@@ -14,17 +14,11 @@ function is_logged_in()
 
 function authenticate_user()
 {
-    // REVIEW: Bad code !!!!! but works though
     $back = app()->registry()->get('back_link');
-    app()->registry()->get('controller')->set_back_link();
     app()->registry()->set('back_link',$back);
-  if (!is_logged_in()) {
-      Session::set('login_back_link',back_link());
-      return redirect_to(users_login_path());
-  }
-  if (!App\Controllers\Users::authenticate(current_user())) {
-      Session::set('login_back_link',back_link());
-      return redirect_to(users_login_path());
-  }
-  return true;
+    if (!auth()->hasIdentity()) {
+        Session::set('login_back_link',back_link());
+        return redirect_to(login_path());
+    }
+    return true;
 }
